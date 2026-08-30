4d02d83e52e1963ead6e98a64ea6ff9d5243fbe4
ASIM <128727510+cryptasim@users.noreply.github.com>
2026-08-30 13:25:26 +0530

    Solve dot-product

---
+++ dot-product/dot-product.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def dot_product(x: list, y: list) -> float:
+    """
+    Returns the dot product as a float.
+    """
+    # Write code here
+    ans = np.dot(x,y)
+    return float(ans)
+    pass
